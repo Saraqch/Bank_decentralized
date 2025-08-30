ea0afe45d3c1a064c1e0df8531b538c6a7da9c4b
fernando <gualterbelmonte@gmail.com>
2025-08-30 18:54:33 -0400

    Feat:add basic deposit/withdraw

--- SmartContracts/contracts/bank/BankManager.sol
+++ SmartContracts/contracts/bank/BankManager.sol
@@ -1,5 +1,40 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+
 contract BankManager {
+    using SafeERC20 for IERC20;
+
+    IERC20 public immutable asset;
+
+    mapping(address => uint256) public balanceOf;
+    uint256 public totalAssets;
+
+    event Deposited(address indexed user, uint256 amount);
+    event Withdrawn(address indexed user, uint256 amount);
+
+    constructor(address asset_) {
+        require(asset_ != address(0), "asset=0");
+        asset = IERC20(asset_);
+    }
+
+    function deposit(uint256 amount) external {
+        require(amount > 0, "amount=0");
+        asset.safeTransferFrom(msg.sender, address(this), amount);
+        balanceOf[msg.sender] += amount;
+        totalAssets += amount;
+        emit Deposited(msg.sender, amount);
+    }
+
+    function withdraw(uint256 amount) external {
+        require(amount > 0, "amount=0");
+        uint256 bal = balanceOf[msg.sender];
+        require(bal >= amount, "insufficient");
+        balanceOf[msg.sender] = bal - amount;
+        totalAssets -= amount;
+        asset.safeTransfer(msg.sender, amount);
+        emit Withdrawn(msg.sender, amount);
+    }
 }
